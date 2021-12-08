69fe285810726ef29cbcb645d050075a8105824a
Alejandro <realpealejo@gmail.com>
2021-12-08 15:18:47 -0500

    structuring project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from './Routes/Home/Home';
+import Quiz from './Routes/Quiz/Quiz';
+import Results from './Routes/Results/Results';
 
 function App() {
+
+  useEffect(() => {
+    
+    axios.get('https://opentdb.com/api_category.php')
+      .then( res => {
+        console.log(res);
+      })
+      .catch(error => console.error(error.message))
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/quiz">
+          <Quiz />
+        </Route>
+        <Route exact path="/results">
+          <Results />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
