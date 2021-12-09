90a459dd6c928cdb088f9f235de64fd03c0b4a76
Alejandro <realpealejo@gmail.com>
2021-12-09 01:02:31 -0500

    results functionality

--- src/Routes/Results/Results.jsx
+++ src/Routes/Results/Results.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useStateValue } from '../../StateManagement/StateProvider';
+import { useHistory } from 'react-router-dom';
 
 function Results() {
+  const [score, setScore] = useState('0/10')
+  const [{results}] = useStateValue();
+  const history = useHistory();
+
+  useEffect(() => {
+    
+    if(results.length === 0){
+      return history.push('/')
+    };
+
+    let counter = 0;
+    results.forEach(element => {element.correct && counter++});
+
+    setScore(`${counter}/10`)
+
+  }, [results, history])
+
   return (
     <div>
       <h1>Results</h1>
+      <h2>You scored</h2>
+      <h2>{score}</h2>
+      <ul>
+        {results &&
+          results.map((result) => (
+            <li key={result.question}>{result.question}</li>
+          ))
+        }
+      </ul>    
     </div>
   )
 }
